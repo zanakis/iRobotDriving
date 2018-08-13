f67a48a0ac7bf2078ea48580831c1cb42ed258ff
Olivier <w.n.w.a.up@gmail.com>
2018-08-13 09:38:29 -0500

    degree check in rplidar_node

--- src/rplidar_node.cpp
+++ src/rplidar_node.cpp
@@ -5,17 +5,18 @@
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-    int count = scan->scan_time / scan->time_increment;
+  int count = scan->scan_time / scan->time_increment;
   //  ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
     //ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
-  
-   for(int i = 0; i < count; i++) {
+  for(int i = 0; i < count; i++) {
+    if(RAD2DEG(scan->angle_min + scan->angle_increment * i) < -60) {
   //      float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
      //  ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
-
+ 
        if(scan->ranges[i]<0.2)
-       ROS_INFO ("dsssssd");
+       ROS_INFO ("dsssssd %f", RAD2DEG(scan->angle_min + scan->angle_increment * i));
     }
+  }
 
    
   
